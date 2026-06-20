5d5ef6716b3d7723fd489231665529595cb14e79
Charlie <20255158@esen.edu.sv>
2026-06-20 07:58:53 -0600

    Implementacion del escenario base

---
+++ parcial1.py
@@ -0,0 +1,57 @@
+#commit 1 - Se implementa la clase Bibliotecario, Recurso, RegistroAtencion, PrestamoLibro y UsoSalaEstudio con sus respectivos atributos y métodos.
+
+class Bibliotecario:
+    def __init__(self, nombre_empleado:str, codigo_usuario:str):
+        self.nombre_empleado = nombre_empleado
+        self.codigo_usuario = codigo_usuario
+
+class Recurso:
+    def __init__(self, codigo_identificador:str, horas_retraso:int, dias_retraso:int):
+        self.codigo_identificador = codigo_identificador
+        self.horas_retraso = 0
+        self.dias_retraso = 0
+
+    def calcular_multa(self, horas_retraso:int, dias_retraso:int, alumnos_espera:int):
+        pass
+
+class RegistroAtencion:
+    def __init__(self, id, carnet_alumno:str, nombre_empleado:str, codigo_usuario:str):
+        self.id = id
+        self.carnet_alumno = carnet_alumno
+        self._recursos :list[Recurso] = []
+        self.bibliotecario = Bibliotecario(nombre_empleado, codigo_usuario)
+
+    @property
+    def recursos(self):
+        return tuple(self._recursos)
+    
+    def ejecutar_auditoria(self, horas_retraso:int, dias_retraso:int, alumnos_espera:int):
+        total_multa = 0
+        for recurso in self._recursos:
+            total_multa += recurso.calcular_multa(horas_retraso, dias_retraso, alumnos_espera)
+        return total_multa
+    
+    def agregar_recurso(self, recurso:Recurso):
+        if len(self._recursos) >= 4:
+            raise ValueError("Limite de recursos por atencion alcanzado")
+        self._recursos.append(recurso)
+
+class PrestamoLibro(Recurso):
+    def __init__(self, codigo_identificador:str, horas_retraso:int, dias_retraso:int):
+        super().__init__(codigo_identificador, horas_retraso, dias_retraso)
+
+    def calcular_multa(self, horas_retraso:int, dias_retraso:int, alumnos_espera:int):
+        total_multa = 0
+        if dias_retraso > 0:
+            total_multa += dias_retraso * 2.50
+        return total_multa
+
+class UsoSalaEstudio(Recurso):
+    def __init__(self, codigo_identificador:str, horas_retraso:int, dias_retraso:int):
+        super().__init__(codigo_identificador, horas_retraso, dias_retraso)
+
+    def calcular_multa(self, horas_retraso:int, dias_retraso:int, alumnos_espera:int):
+        total_multa = 0
+        if horas_retraso > 0:
+            total_multa += horas_retraso * (alumnos_espera / 1000)
+        return total_multa
